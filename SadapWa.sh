cc7973c4fcb2a88f1c0b5e755f9c2fd1e9fb4d40
ARIF - 404 <78012268+termxteam@users.noreply.github.com>
2021-02-15 21:35:00 +0700

    Add files via upload

---
+++ SadapWa.sh
@@ -0,0 +1,158 @@
+
+#SadapWA
+#Coded by. MR ARIF
+#Jangan recode ngentod!!
+#mau recode izin +62822 6027 6155
+
+echo $cy"<|=======================================|>"
+echo $cy "||     $i Sadap WhatsApp Version 2.0     $cy||"
+echo $cy "||     $i Author: MR ARIF                $cy||"
+echo $cy "||     $i Team  : Termux Cyber Team  $cy||"
+echo $cy"<|=======================================|>"
+echo
+sleep 4
+echo $i"<[===============================]>"
+echo $i"<[  $me 0.Exit This Tool $i           ]>"
+echo $i"<[  $pu 1.Sadap Nomor Telkomsel $i    ]>"
+echo $i"<[  $me 2.Sadap Nomor XL   $i         ]>"
+echo $i"<[  $pu 3.Sadap Nomor Three   $i      ]>"
+echo $i"<[  $me 4.Sadap Nomor Axis    $i      ]>"
+echo $i"<[  $pu 5.Sadap Nomor Smartfren  $i   ]>"
+echo $i"<[===============================]>"
+echo
+echo $bi "╭─"$i"Pilih Salah Satu"
+read -p " ╰─────────$MR ARIF>>"  pil
+
+if [ $pil = '1' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MR ARIF>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+sh TOD.sh
+fi
+
+if [ $pil = '3' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MR ARIF>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+sh TOD.sh
+fi
+
+if [ $pil = '4' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MR ARIF>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+sh TOD.sh
+fi
+
+if [ $pil = '0' ]
+then
+clear
+echo $cy"Thanks for using this tools"
+sleep 2
+echo $cy"Silahkan menunggu Besok/lusa/selamanya untuk mendapatkan kode verifikasi Lu Sangka Sadap Wa Gampang Tolol lu Bangsad Awok Kawok Sabar Aja lu wkkwkwkw:))"
+sleep 1
+echo
+toilet -f future HAHAHAHAH | lolcat
+echo
+echo $me"Contact me kalo mau complain : $i +62822 6027 6155"
+exit
+fi
+
